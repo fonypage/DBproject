4d85854d22f546f17983afb01953ad670690fd9d
artem <evko_artem@mail.ru>
2024-11-19 18:58:30 +0300

    start

---
+++ src/main/java/com/library/bd/DBproject/DBprojectApplication.java
@@ -0,0 +1,13 @@
+package com.library.bd.DBproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DBprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/library/bd/DBproject/DBprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.bd.DBproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DBprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
