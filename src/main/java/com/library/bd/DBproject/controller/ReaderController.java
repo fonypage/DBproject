a9da1e95a0c38806b2957b1a72d6bf9169506cdb
artem <evko_artem@mail.ru>
2024-11-20 18:59:52 +0300

    start

---
+++ src/main/java/com/library/bd/DBproject/controller/ReaderController.java
@@ -0,0 +1,9 @@
+package com.library.bd.DBproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+public class ReaderController {
+}
